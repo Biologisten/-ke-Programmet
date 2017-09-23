c28d84bacae3d4333e0dcac267ec6869856ba889
Oscar Andersson Engler <oscar.engler@allt3.se>
2017-09-23 21:34:08 +0200

    Created rough UI

---
+++ Åke-programmet/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Åke_programmet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
