e52494d363d2fc8b341d0cc698e695257f942f78
Biologisten <oscar.engler@allt3.se>
2018-07-22 16:55:31 +0200

    Didn't do nuffing

--- Åke-programmet/MainWindow.xaml.cs
+++ Åke-programmet/MainWindow.xaml.cs
@@ -30,39 +30,47 @@ namespace Åke_programmet
 
         private void Set_values(string id)
         {
-            Library.Data Sample = test.Get_info(id);
+            if (test.Get_info(id) != null)
+            {
+                Library.Data Sample = test.Get_info(id);
 
-            PRODUKTNAMN.Text = Sample.PRODUKTNAMN;
-            PRODUKTNUMMER.Text = Sample.PRODNUM;
-            KULÖR.Text = Sample.KULÖR;
-            SAPNUM.Text = Sample.SAPNUM;
-            SAPSATNUMMER.Text = Sample.SAPSATNUM;
-            J1_a.Text = "1";
-            J1_b.Text = "1";
-            J2_a.Text = "1";
-            J2_b.Text = "1";
-            J3_a.Text = "1";
-            J3_b.Text = "1";
-            J4_a.Text = "1";
-            J4_b.Text = "1";
-            N1_a.Text = "1";
-            N1_b.Text = "1";
-            N2_a.Text = "1";
-            N2_b.Text = "1";
-            N3_a.Text = "1";
-            N3_b.Text = "1";
-            N4_a.Text = "1";
-            N4_b.Text = "1";
-            DENSITET.Text = "1";
-            UTG_DENS.Text = "1";
-            CoP_DIN.Text = "1";
-            UT_CoP.Text = "1";
-            RIVN.Text = "1";
-            GL85.Text = "1";
-            UGL85.Text = "1";
-            PH.Text = "1";
-            UT_PH.Text = "1";
-            
+                PRODUKTNAMN.Text = Sample.PRODUKTNAMN;
+                PRODUKTNUMMER.Text = Sample.PRODNUM;
+                KULÖR.Text = Sample.KULÖR;
+                SAPNUM.Text = Sample.SAPNUM;
+                SAPSATNUMMER.Text = Sample.SAPSATNUM;
+                J1_a.Text = "1";
+                J1_b.Text = "1";
+                J2_a.Text = "1";
+                J2_b.Text = "1";
+                J3_a.Text = "1";
+                J3_b.Text = "1";
+                J4_a.Text = "1";
+                J4_b.Text = "1";
+                N1_a.Text = "1";
+                N1_b.Text = "1";
+                N2_a.Text = "1";
+                N2_b.Text = "1";
+                N3_a.Text = "1";
+                N3_b.Text = "1";
+                N4_a.Text = "1";
+                N4_b.Text = "1";
+                DENSITET.Text = "1";
+                UTG_DENS.Text = "1";
+                CoP_DIN.Text = "1";
+                UT_CoP.Text = "1";
+                RIVN.Text = "1";
+                GL85.Text = "1";
+                UGL85.Text = "1";
+                PH.Text = "1";
+                UT_PH.Text = "1";
+
+            }
+        }
+
+        private void Listsource()
+        {
+            Listview.ItemsSource = test.DBlist;
         }
 
         private void Listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
