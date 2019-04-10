abcaed5b2379ff7886d8841eff01ae5156312a07
Biologisten <oscar.engler@bahnhof.se>
2019-04-10 19:23:48 +0200

    Recipes are now saveable

--- Åke-programmet/MainWindow.xaml.cs
+++ Åke-programmet/MainWindow.xaml.cs
@@ -25,8 +25,8 @@ namespace Åke_programmet
         public MainWindow()
         {
             InitializeComponent();
-            test.PrintDept();
-            Listsource();
+            test.PrintDept();   //Felsökning, testar anslutningen och visar värdena i databasen
+            Listsource();       //Ger källan till receptlistan
         }
 
         private void Set_values(string id)
@@ -108,7 +108,6 @@ namespace Åke_programmet
 
         private void Listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //Set_values(sender.ToString());
             Set_values(Listview.SelectedValue.ToString());
         }
 
@@ -126,72 +125,8 @@ namespace Åke_programmet
 
         private void Save_Recipe(object sender, RoutedEventArgs e)
         {
-            Library.Data recipe = new Library.Data(SATSNUM.Text, PRODUKTNAMN.Text, PRODUKTNUMMER.Text, KULÖR.Text, SAPNUM.Text, SAPSATNUMMER.Text, J1_a.Text, J1_b.Text, J2_a.Text, J2_b.Text, J3_a.Text, J3_b.Text, J4_a.Text, J4_b.Text, N1_a.Text, N1_b.Text, N2_a.Text, N2_b.Text, N3_a.Text, N3_b.Text, N4_a.Text, N4_b.Text, DE.Text, UTG_DENS.Text, CoP_DIN.Text, UT_CoP.Text, RIVN.Text, GL85.Text, UGL85.Text, PH.Text, UT_PH.Text, ROT.Text, UTG_ROT.Text, GL60.Text, UGL60.Text, DE.Text, DL.Text, DA.Text, DB.Text, FST.Text, UFST.Text, BORT.Text, UTSKRDAT.Text, STARTDAT.Text, GODKDAT.Text, TAPPDAT.Text, REC_VIKT.Text, SATS_VIKT.Text, SATS_VOL.Text, TAPP_VIKT.Text, TAPP_VOL.Text);
-            test.DBlist.Add();
-
-            SATSNUM.Text
-            PRODUKTNAMN.Text = Values.PRODUKTNAMN;
-            PRODUKTNUMMER.Text = Values.PRODNUM;
-            KULÖR.Text = Values.KULÖR;
-            SAPNUM.Text = Values.SAPNUM;
-            SAPSATNUMMER.Text = Values.SAPSATNUM;
-            J1_a.Text = Values.JUSTERING1_A;
-            J1_b.Text = Values.JUSTERING1_B;
-            J2_a.Text = Values.JUSTERING2_A;
-            J2_b.Text = Values.JUSTERING2_B;
-            J3_a.Text = Values.JUSTERING3_A;
-            J3_b.Text = Values.JUSTERING3_B;
-            J4_a.Text = Values.JUSTERING4_A;
-            J4_b.Text = Values.JUSTERING4_B;
-            N1_a.Text = Values.NYANSERING1_A;
-            N1_b.Text = Values.NYANSERING1_B;
-            N2_a.Text = Values.NYANSERING2_A;
-            N2_b.Text = Values.NYANSERING2_B;
-            N3_a.Text = Values.NYANSERING3_A;
-            N3_b.Text = Values.NYANSERING3_B;
-            N4_a.Text = Values.NYANSERING4_A;
-            N4_b.Text = Values.NYANSERING4_B;
-            DENSITET.Text = Values.DENSITET;
-            UTG_DENS.Text = Values.UTGDENSITET;
-            CoP_DIN.Text = Values.COPDIN;
-            UT_CoP.Text = Values.UTCOP;
-            RIVN.Text = Values.RIVN;
-            GL85.Text = Values.GL85;
-            UGL85.Text = Values.UGL85;
-            PH.Text = Values.PH;
-            UT_PH.Text = Values.UTPH;
-            ROT.Text = Values.ROT;
-            UTG_ROT.Text = Values.UTGROT;
-            GL60.Text = Values.GL60;
-            UGL60.Text = Values.UGL60;
-            DE.Text = Values.DE;
-            DL.Text = Values.DL;
-            DA.Text = Values.DA;
-            DB.Text = Values.DB;
-            FST.Text = Values.FST;
-            UFST.Text = Values.UFST;
-            BORT.Text = Values.BORT;
-            UTSKRDAT.Text = Values.UTSKRDAT;
-            STARTDAT.Text = Values.STARTDAT;
-            GODKDAT.Text = Values.GODKDAT;
-            TAPPDAT.Text = Values.TAPPDAT;
-            REC_VIKT.Text = Values.RECVIKT;
-            SATS_VIKT.Text = Values.SATSVIKT;
-            SATS_VOL.Text = Values.SATSVOL;
-            TAPP_VIKT.Text = Values.TAPPVIKT;
-            TAPP_VOL.Text = Values.TAPPVOL;
-            UTBYTE.Text = Values.UTBYTE;
-            TANK.Text = Values.TANK;
-            CM.Text = Values.CM;
-            UPM_VOL.Text = Values.UPMVOL;
-            STATUS.Text = Values.STATUS;
-            VER.Text = Values.VER;
-            JUST.Text = Values.JUST;
-            NYANS.Text = Values.NYANS;
-            TILLVTID.Text = Values.TILLVTID;
-            KONTRTID.Text = Values.KONTRTID;
-            ANMÄRKNING.Text = Values.ANMÄRKNING;
-            KOD_BOK.Text = Values.KOD_BOK;
+            Library.Data recipe = new Library.Data(SATSNUM.Text, PRODUKTNAMN.Text, PRODUKTNUMMER.Text, KULÖR.Text, SAPNUM.Text, SAPSATNUMMER.Text, J1_a.Text, J1_b.Text, J2_a.Text, J2_b.Text, J3_a.Text, J3_b.Text, J4_a.Text, J4_b.Text, N1_a.Text, N1_b.Text, N2_a.Text, N2_b.Text, N3_a.Text, N3_b.Text, N4_a.Text, N4_b.Text, DENSITET.Text, UTG_DENS.Text, CoP_DIN.Text, UT_CoP.Text, RIVN.Text, GL85.Text, UGL85.Text, PH.Text, UT_PH.Text, ROT.Text, UTG_ROT.Text, GL60.Text, UGL60.Text, DE.Text, DL.Text, DA.Text, DB.Text, FST.Text, UFST.Text, BORT.Text, UTSKRDAT.Text, STARTDAT.Text, GODKDAT.Text, TAPPDAT.Text, REC_VIKT.Text, SATS_VIKT.Text, SATS_VOL.Text, TAPP_VIKT.Text, TAPP_VOL.Text, UTBYTE.Text, TANK.Text, CM.Text, UPM_VOL.Text, STATUS.Text, VER.Text, JUST.Text, NYANS.Text, TILLVTID.Text, KONTRTID.Text, ANMÄRKNING.Text, KOD_BOK.Text);
+            test.DBlist.Add(recipe);
         }
     }
 }
