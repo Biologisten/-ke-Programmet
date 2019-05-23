6cd3ae6e553733af2458fd2801345e6294ef96fe
Biologisten <oscar.engler@bahnhof.se>
2019-05-23 09:53:29 +0200

    Listview fix
    Made Listview Selection_Changed work again after it's rework.
    Made Set_values a overload, now it takes both string and int.

--- Åke-programmet/MainWindow.xaml.cs
+++ Åke-programmet/MainWindow.xaml.cs
@@ -101,6 +101,78 @@ namespace Åke_programmet
             }
         }
 
+        private void Set_values(int id)  //Sets values from DB to boxes in UI
+        {
+            if (test.Get_info(id.ToString()) != null)
+            {
+                Library.Data Values = test.Get_info(id.ToString());
+
+                SATSNUM.Text = Values.SATSNUM;
+                PRODUKTNAMN.Text = Values.PRODUKTNAMN;
+                PRODUKTNUMMER.Text = Values.PRODNUM;
+                KULÖR.Text = Values.KULÖR;
+                SAPNUM.Text = Values.SAPNUM;
+                SAPSATNUMMER.Text = Values.SAPSATNUM;
+                J1_a.Text = Values.JUSTERING1_A;
+                J1_b.Text = Values.JUSTERING1_B;
+                J2_a.Text = Values.JUSTERING2_A;
+                J2_b.Text = Values.JUSTERING2_B;
+                J3_a.Text = Values.JUSTERING3_A;
+                J3_b.Text = Values.JUSTERING3_B;
+                J4_a.Text = Values.JUSTERING4_A;
+                J4_b.Text = Values.JUSTERING4_B;
+                N1_a.Text = Values.NYANSERING1_A;
+                N1_b.Text = Values.NYANSERING1_B;
+                N2_a.Text = Values.NYANSERING2_A;
+                N2_b.Text = Values.NYANSERING2_B;
+                N3_a.Text = Values.NYANSERING3_A;
+                N3_b.Text = Values.NYANSERING3_B;
+                N4_a.Text = Values.NYANSERING4_A;
+                N4_b.Text = Values.NYANSERING4_B;
+                DENSITET.Text = Values.DENSITET;
+                UTG_DENS.Text = Values.UTGDENSITET;
+                CoP_DIN.Text = Values.COPDIN;
+                UT_CoP.Text = Values.UTCOP;
+                RIVN.Text = Values.RIVN;
+                GL85.Text = Values.GL85;
+                UGL85.Text = Values.UGL85;
+                PH.Text = Values.PH;
+                UT_PH.Text = Values.UTPH;
+                ROT.Text = Values.ROT;
+                UTG_ROT.Text = Values.UTGROT;
+                GL60.Text = Values.GL60;
+                UGL60.Text = Values.UGL60;
+                DE.Text = Values.DE;
+                DL.Text = Values.DL;
+                DA.Text = Values.DA;
+                DB.Text = Values.DB;
+                FST.Text = Values.FST;
+                UFST.Text = Values.UFST;
+                BORT.Text = Values.BORT;
+                UTSKRDAT.Text = Values.UTSKRDAT;
+                STARTDAT.Text = Values.STARTDAT;
+                GODKDAT.Text = Values.GODKDAT;
+                TAPPDAT.Text = Values.TAPPDAT;
+                REC_VIKT.Text = Values.RECVIKT;
+                SATS_VIKT.Text = Values.SATSVIKT;
+                SATS_VOL.Text = Values.SATSVOL;
+                TAPP_VIKT.Text = Values.TAPPVIKT;
+                TAPP_VOL.Text = Values.TAPPVOL;
+                UTBYTE.Text = Values.UTBYTE;
+                TANK.Text = Values.TANK;
+                CM.Text = Values.CM;
+                UPM_VOL.Text = Values.UPMVOL;
+                STATUS.Text = Values.STATUS;
+                VER.Text = Values.VER;
+                JUST.Text = Values.JUST;
+                NYANS.Text = Values.NYANS;
+                TILLVTID.Text = Values.TILLVTID;
+                KONTRTID.Text = Values.KONTRTID;
+                ANMÄRKNING.Text = Values.ANMÄRKNING;
+                KOD_BOK.Text = Values.KOD_BOK;
+            }
+        }
+
         private void Listsource()
         {
             List<Library.Database.User> items = test.DBinfo();
@@ -109,18 +181,19 @@ namespace Åke_programmet
 
         private void Listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Set_values(Listview.SelectedIndex.ToString());
+            int n = Listview.SelectedIndex + 1;
+            Set_values(n);
         }
 
         private void SATSNUM_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.Key != Key.Enter)
+            if (e.Key == Key.Enter)
             {
-                return;
+                Set_values(SATSNUM.Text);
             }
             else
             {
-                Set_values(SATSNUM.Text);
+                return;
             }
         }
 
